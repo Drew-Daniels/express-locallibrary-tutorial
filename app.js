6ef24ed09e10e7cd636944f560f4c0dd9dc4add8
Drew Daniels <drew@thedaniels.com>
2022-03-14 13:52:15 -0500

    Updated db database connection string.

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ var logger = require('morgan');
 
 // set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://m001-student:1QcirL2wvvUwusv9@sandbox.cwhyp.mongodb.net/local_library?retryWrites=true&w=majority';
+var dev_db_url = 'mongodb+srv://m001-student:1QcirL2wvvUwusv9@sandbox.cwhyp.mongodb.net/local_library?retryWrites=true&w=majority';
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
